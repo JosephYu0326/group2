f288b1ce4d70b6019bca142df78a4e8deca2e470
Joseph <pkujm951@gmail.com>
2022-03-17 23:19:02 +0800

    版面調整

--- add_museum.php
+++ add_museum.php
@@ -68,81 +68,89 @@ try {
 </style>
 </style>
 <div class="content-wrapper">
-    <div class="row justify-content-center">
-        <div class="col-lg-6  mt-3 ">
-            <div class="card text-white bg-dark">
-                <div class="card-body">
-                    <h5 class="card-title">Add Museum</h5>
-                    <form name="form1" method="post" novalidate onsubmit="checkForm(); return false;">
-                        <div class="mb-3">
-                            <label for="museum_name" class="form-label">館名</label>
-                            <input type="text" class="form-control" id="museum_name" name="museum_name" required>
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="museum_location_id" class="form-label">位置</label>
-                            <select class="form-select" aria-label="Default select example" name="museum_location_id">
-                                <option selected>-- select Location --</option>
-                                <?php foreach ($results as $output) { ?>
-                                    <option value="<?php echo $output["museum_location_id"]; ?>"><?php echo $output["museum_direction"]; ?>-<?php echo $output["Museum_city"]; ?> </option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <!-- <div class="mb-3">
+    <div class="content">
+        <div class="row justify-content-center">
+            <div class="col-xs-12  mt-3 ">
+                <div class="box ">
+                    <div class="box-header">
+                        <h5 class="card-title">Add Museum</h5>
+                    </div><br>
+                    <div class="box-body">
+                        <form name="form1" method="post" novalidate onsubmit="checkForm(); return false;">
+                            <div class="main-form">
+                                <div class="mb-3">
+                                    <label for="museum_name" class="form-label">館名</label>
+                                    <input type="text" class="form-control" id="museum_name" name="museum_name" required>
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="museum_location_id" class="form-label">位置</label>
+                                    <select class="form-select" aria-label="Default select example" name="museum_location_id">
+                                        <option selected>-- select Location --</option>
+                                        <?php foreach ($results as $output) { ?>
+                                            <option value="<?php echo $output["museum_location_id"]; ?>"><?php echo $output["museum_direction"]; ?>-<?php echo $output["Museum_city"]; ?> </option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- <div class="mb-3">
                             <label for="museum_location_id" class="form-label">位置</label>
                             <input type="text" class="form-control" id="museum_location_id" name="museum_location_id">
                             <div class="form-text"></div>
                         </div> -->
-                        <div class="mb-3">
-                            <label for="Museum_kind_id" class="form-label">類型</label>
-                            <select class="form-select" aria-label="Default select example" name="Museum_kind_id">
-                                <option selected>-- select kind --</option>
-                                <?php foreach ($results1 as $output) { ?>
-                                    <option value="<?php echo $output["museum_kind_id"]; ?>"><?php echo $output["museum_kind"]; ?> </option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <!-- <div class="mb-3">
+                                <div class="mb-3">
+                                    <label for="Museum_kind_id" class="form-label">類型</label>
+                                    <select class="form-select" aria-label="Default select example" name="Museum_kind_id">
+                                        <option selected>-- select kind --</option>
+                                        <?php foreach ($results1 as $output) { ?>
+                                            <option value="<?php echo $output["museum_kind_id"]; ?>"><?php echo $output["museum_kind"]; ?> </option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                                <!-- <div class="mb-3">
                             <label for="Museum_kind_id" class="form-label">類型</label>
                             <input type="text" class="form-control" id="Museum_kind_id" name="Museum_kind_id">
                             <div class="form-text"></div>
                         </div> -->
-                        <div class="mb-3">
-                            <label for="Museum_features" class="form-label">特色</label>
-                            <textarea class="form-control" name="Museum_features" id="Museum_features" cols="30" rows="30"></textarea>
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="Museum_introduce" class="form-label">詳細介紹</label>
-                            <textarea class="form-control" name="Museum_introduce" id="Museum_introduce" cols="30" rows="30"></textarea>
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="Museum_booking_notice" class="form-label">購票須知</label>
-                            <textarea class="form-control" name="Museum_booking_notice" id="Museum_booking_notice" cols="30" rows="30"></textarea>
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="Museum_more_information" class="form-label">更多資訊</label>
-                            <textarea class="form-control" name="Museum_more_information" id="Museum_more_information" cols="30" rows="30"></textarea>
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <input type="hidden" id="pic" name="pic">
-                            <img src="" alt="" id="myimg" style="width: 100%;">
-                            <button type="button" onclick="avatar.click()">上傳照片</button>
-                        </div>
-                        <button type="submit" class="btn btn-primary">新增</button>
-                    </form>
+                                <div class="mb-3">
+                                    <label for="Museum_features" class="form-label">特色</label>
+                                    <textarea class="form-control" name="Museum_features" id="Museum_features" cols="30" rows="30"></textarea>
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="Museum_introduce" class="form-label">詳細介紹</label>
+                                    <textarea class="form-control" name="Museum_introduce" id="Museum_introduce" cols="30" rows="30"></textarea>
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="Museum_booking_notice" class="form-label">購票須知</label>
+                                    <textarea class="form-control" name="Museum_booking_notice" id="Museum_booking_notice" cols="30" rows="30"></textarea>
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="Museum_more_information" class="form-label">更多資訊</label>
+                                    <textarea class="form-control" name="Museum_more_information" id="Museum_more_information" cols="30" rows="30"></textarea>
+                                    <div class="form-text"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <input type="hidden" id="pic" name="pic">
+                                    <img src="" alt="" id="myimg" style="width: 100%;">
+                                    <button class="btn btn-dark" type="button" onclick="avatar.click()">上傳照片</button>
+                                </div>
+                                <div class="mb-3 d-grid gap-2">
+                                    <button type="submit" class="btn btn-dark">新增</button>
+                                </div>
+                        </form>
+                    </div>
 
                     <form name="image_form" onsubmit="return false;" style="display: none;">
                         <input type="file" id="avatar" name="avatar" accept="image/*">
                     </form>
-
                 </div>
+
             </div>
         </div>
     </div>
+</div>
 </div>
 
 <?php include __DIR__ . '/parts/scripts.php'; ?>
@@ -178,7 +186,7 @@ try {
                     console.log(obj);
                     if (obj.success) {
                         alert('新增成功');
-                        location.href='museum_list.php';
+                        location.href = 'museum_list.php';
                     } else {
                         alert('新增失敗');
                     }
@@ -189,19 +197,20 @@ try {
 </script>
 <script>
     const avatar = document.querySelector('#avatar');
-    function sendData(){
+
+    function sendData() {
         const fd2 = new FormData(document.image_form);
 
-        fetch('upload.php',{
-            method:'POST',
-            body: fd2
-        }).then(r=>r.json())
-        .then(obj=>{
-            if(obj.success && obj.filename){
-                myimg.src = './imgs/' + obj.filename;
-                pic.value = obj.filename;
-            }
-        })
+        fetch('upload.php', {
+                method: 'POST',
+                body: fd2
+            }).then(r => r.json())
+            .then(obj => {
+                if (obj.success && obj.filename) {
+                    myimg.src = './imgs/' + obj.filename;
+                    pic.value = obj.filename;
+                }
+            })
     }
     avatar.onchange = sendData;
 </script>
