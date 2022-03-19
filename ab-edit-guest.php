fc4b14be7e7247877a754561b3c6458d142f1c51
chuan <chihchuan.hsu316@gmail.com>
2022-03-20 02:13:40 +0800

    新增圖片 和選擇活動選項 跳轉回list

--- ab-edit-guest.php
+++ ab-edit-guest.php
@@ -10,68 +10,97 @@ $sql = "SELECT * FROM activity_guest WHERE Activity_Guest_id=$sid";
 $row = $pdo->query($sql)->fetch();
 
 
-if(empty($row)){
+if (empty($row)) {
     header('Location: ab-list-guest.php'); // 找不到資炓轉向列表頁
     exit;
 }
+
+//活動選擇
+$stmt = $pdo->query("SELECT * FROM activity ORDER BY Activity_id");
+$raw_data = $stmt->fetchAll();
+
+$Activity_id = [];
+
+foreach ($raw_data as $r) {
+    if ($r['Activity_id'] != '') {
+        $Activity_id[] = $r;
+    }
+}
+
+
+
 ?>
 <?php include __DIR__ . '/parts/html-head.php'; ?>
 <?php include __DIR__ . '/parts/navbar.php'; ?>
 <style>
-    form .mb-3 .form-text{
+    form .mb-3 .form-text {
         color: red;
     }
-
 </style>
 <div class="container">
     <div class="row">
         <div class="col-lg-6">
             <div class="card">
                 <div class="card-body">
+
+                    <form name="avatar_form" onsubmit="return false;" style="display: none;">
+                        <input type="file" id="avatar" name="avatar" accept="image/jpeg,image/png">
+                    </form>
+
+
                     <h5 class="card-title">修改資料</h5>
                     <form name="form1" method="post" novalidate onsubmit="checkForm(); return false;">
                         <input type="hidden" name="Activity_Guest_id" value="<?= $row['Activity_Guest_id'] ?>">
 
-                       
+
 
                         <div class="mb-3">
                             <label for="name" class="form-label">* 嘉賓名稱</label>
-                            <input type="text" class="form-control" id="name" name="name" required
-                            value="<?= htmlentities($row['Activity_Guest_Name']) ?>">
+                            <input type="text" class="form-control" id="name" name="name" required value="<?= htmlentities($row['Activity_Guest_Name']) ?>">
                             <div class="form-text"></div>
                         </div>
-                        <div class="mb-3">
+
+                        <input type="hidden" id="img" name="img">
+
+                        <br>
+                        <img src="" alt="" id="myimg" width="200px">
+                        <br>
+                        <button type="button" class="btn btn-primary" onclick="avatar.click()">上傳嘉賓圖片</button>
+
+
+                        <!-- <div class="mb-3">
                             <label for="img" class="form-label">* 嘉賓圖片</label>
-                            <input type="text" class="form-control" id="img" name="img" required
-                            value="<?= htmlentities($row['Activity_Guest_Img']) ?>">
+                            <input type="text" class="form-control" id="img" name="img" required value="<?= htmlentities($row['Activity_Guest_Img']) ?>">
                             <div class="form-text"></div>
-                        </div>
+                        </div> -->
                         <div class="mb-3">
                             <label for="Profession" class="form-label">* 嘉賓職業</label>
-                            <input type="text" class="form-control" id="Profession" name="Profession" required
-                            value="<?= htmlentities($row['Activity_Guest_Job_Title']) ?>">
+                            <input type="text" class="form-control" id="Profession" name="Profession" required value="<?= htmlentities($row['Activity_Guest_Job_Title']) ?>">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="company" class="form-label">* 嘉賓公司</label>
-                            <input type="text" class="form-control" id="company" name="company" required
-                            value="<?= htmlentities($row['Activity_Guest_Company_name']) ?>">
+                            <input type="text" class="form-control" id="company" name="company" required value="<?= htmlentities($row['Activity_Guest_Company_name']) ?>">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="a-url" class="form-label">* 嘉賓網址</label>
-                            <input type="url" class="form-control" id="a-url" name="a-url" required
-                            value="<?= htmlentities($row['Activity_Guest_Profiles']) ?>">
+                            <input type="url" class="form-control" id="a-url" name="a-url" required value="<?= htmlentities($row['Activity_Guest_Profiles']) ?>">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="text" class="form-label">嘉賓介紹</label>
-                            <textarea class="form-control" name="text"
-                            id="text" 
-                            cols="30" 
-                            rows="3"><?= $row['Activity_Guest_URL'] ?></textarea>
+                            <textarea class="form-control" name="text" id="text" cols="30" rows="3"><?= $row['Activity_Guest_URL'] ?></textarea>
 
                             <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">嘉賓要參加的活動
+                            <select class="form-select" name="Activity_id" aria-label="Default select example">
+                                <?php foreach ($Activity_id as $ai) : ?>
+                                    <option value="<?= $ai['Activity_id'] ?>"><?= $ai['Activity_Name'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
 
 
@@ -90,43 +119,41 @@ if(empty($row)){
 </div>
 <?php include __DIR__ . '/parts/scripts.php'; ?>
 <script>
-    
-
     const name = document.form1.name;
     const name_msg = name.closest('.mb-3').querySelector('.form-text');
 
-    function checkForm(){
+    function checkForm() {
         let isPass = true; // 有沒有通過檢查
 
-        name_msg.innerText = '';  // 清空訊息
-       
+        name_msg.innerText = ''; // 清空訊息
+
 
         // TODO: 表單資料送出之前, 要做格式檢查
 
-        if(name.value.length<2){
+        if (name.value.length < 2) {
             isPass = false;
             name_msg.innerText = '請填寫正確的姓名'
         }
 
-        
 
-        if(isPass){
+
+        if (isPass) {
             const fd = new FormData(document.form1);
 
             fetch('ab-edit-guest-api.php', {
-                method: 'POST',
-                body: fd
-            }).then(r => r.json())
-            .then(obj => {
-                console.log(obj);
-                if(obj.success){
-                    alert('修改成功');
-                    // location.href = 'ab-list.php';
-                } else {
-                    alert('沒有修改');
-                }
+                    method: 'POST',
+                    body: fd
+                }).then(r => r.json())
+                .then(obj => {
+                    console.log(obj);
+                    if (obj.success) {
+                        alert('修改成功');
+                         location.href = 'ab-list-guest.php';
+                    } else {
+                        alert('沒有修改');
+                    }
 
-            })
+                })
 
 
         }
@@ -134,7 +161,26 @@ if(empty($row)){
 
     }
 
+      // 檢查圖片
+      function sendData() {
+        const fd = new FormData(document.avatar_form);
 
+        fetch('upload-avatar.php', {
+                method: 'POST',
+                body: fd
+            }).then(r => r.json())
+            .then(obj => {
+                console.log(obj);
+                if (obj.success && obj.filename) {
+                    myimg.src = './imgs/' + obj.filename;
+                    img.value = obj.filename;
+                }
+            });
+    }
+
+    avatar.onchange = sendData;
+    // ---
+    
 </script>
 <?php include __DIR__ . '/parts/html-foot.php'; ?>
 
